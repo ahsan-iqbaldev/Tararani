f08f41b72162dc06c6e0e0472b16e5831590bf47
Ahsan Iqbal <codebyahsan@gmail.com>
2024-05-12 17:09:45 +0500

    handle product detail page

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import image from "../assets/51eg55uWmdL._AC_UX679_.jpg";
 import { useParams } from "react-router-dom";
+import { getSingleProduct } from "../store/actions/productsAction";
+import { useDispatch, useSelector } from "react-redux";
 
 const ProductDetail = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { singleProduct } = useSelector((state) => state.Products);
   const [selectedColor, setSelectedColor] = useState("");
-  const {id} = useParams()
-  console.log(id,"id")
 
   const handleColorSelect = (color) => {
     setSelectedColor(color);
   };
+  useEffect(() => {
+    dispatch(getSingleProduct(id));
+  }, []);
   return (
     <section className="text-gray-700 body-font overflow-hidden bg-white">
       <div className="container px-2 py-24 mx-auto">
@@ -17,14 +23,11 @@ const ProductDetail = () => {
           <img
             alt="ecommerce"
             className="lg:w-1/3 md:w-full rounded border border-gray-200 h-[300px] md:h-[450px]"
-            src={image}
+            src={singleProduct?.productImages[0]}
           />
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              BRAND NAME
-            </h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-              The Catcher in the Rye
+              {singleProduct?.title}
             </h1>
             <div className="flex mb-4">
               <span className="flex items-center">
@@ -86,18 +89,20 @@ const ProductDetail = () => {
                 <span className="text-gray-600 ml-3">4 Reviews</span>
               </span>
               <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
-              <span className="title-font font-medium text-2xl text-gray-900 md:ml-9">
-                  $58.00
+                <span className="title-font font-medium text-2xl text-gray-900 md:ml-9">
+                  Rs.{singleProduct?.price}
+                </span>
+                <span className="title-font font-medium text-1xl text-gray-900 md:ml-2 md:mt-1 line-through">
+                  Rs.{singleProduct?.comparePrice}
                 </span>
               </span>
             </div>
             <p className="leading-relaxed">
-              Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-              sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-              juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-              seitan poutine tumeric. Gastropub blue bottle austin listicle
-              pour-over, neutra jean shorts keytar banjo tattooed umami
-              cardigan.
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `<p className="text-sm">${singleProduct?.description}</p>`,
+                }}
+              />
             </p>
             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
               <div className="flex">
