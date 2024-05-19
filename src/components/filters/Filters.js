8aaba6d2c7be2905d8a69052301623ab37ba421a
Ahsan Iqbal <revensolutions01@gmail.com>
2024-05-19 21:00:37 +0500

    work on filter UI

---
+++ src/components/filters/Filters.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import FormInput from "./FormInput";
+import { Form, Link } from "react-router-dom";
+import FormRange from "./FormRange";
+import FormSelect from "./FormSelect";
+import FormDatePicker from "./FormDatePicker";
+import FormCheckbox from "./FormCheckbox";
+
+const Filters = () => {
+  const [selectCategoryList, setSelectCategoryList] = useState([
+    "all",
+    "shoes",
+    "slippers",
+    "heels",
+    "t-shirts",
+    "jackets",
+    "caps",
+    "shorts",
+    "sweaters",
+    "sneakers",
+    "shirts",
+    "boots",
+    "overshirts",
+    "pants",
+    "jeans",
+    "socks",
+    "belts",
+    "trainers",
+  ]);
+  const [selectBrandList, setSelectBrandList] = useState([
+    "all",
+    "WALK LONDON",
+    "Reebok",
+    "Nike",
+    "Jack & Jones",
+    "Crocs",
+    "Vans",
+    "Puma",
+    "New Balance",
+    "Tommy Jeans",
+    "Tommy Hilfiger",
+    "Bershka",
+    "New Look",
+    "AllSaints",
+    "Columbia",
+    "The North Face",
+    "Collusion",
+    "ASOS DESIGN",
+    "Topman",
+    "Dr Denim",
+    "Polo Ralph Lauren",
+    "ASOS Dark Future",
+    "Levi's",
+    "Threadbare",
+    "Calvin Klein",
+    "AAPE BY A BATHING APE®",
+    "Good For Nothing",
+    "Timberland",
+    "Pull and Bear",
+    "Koi Footwear",
+    "adidas performance",
+    "Nike Running",
+    "Dr Martens",
+    "River Island",
+  ]);
+
+  return (
+    <Form className="bg-gray-200 rounded-md px-8 py-4 grid gap-x-4  gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* SEARCH */}
+      <FormInput
+        type="search"
+        label="search product"
+        name="search"
+        size="input-sm"
+        defaultValue=""
+      />
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        list={selectCategoryList}
+        size="select-sm"
+        defaultValue="all"
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label="select brand"
+        name="brand"
+        list={selectBrandList}
+        size="select-sm"
+        defaultValue="all"
+      />
+      {/* ORDER */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={["asc", "desc", "price high", "price low"]}
+        size="select-sm"
+        defaultValue="a-z"
+      />
+      {/* Producer */}
+      <FormSelect
+        label="Select gender"
+        name="gender"
+        list={["all", "male", "female"]}
+        size="select-sm"
+        defaultValue="all"
+      />
+      {/* PRICE */}
+      <FormRange
+        name="price"
+        label="select price"
+        size="range-sm"
+        price={2000}
+      />
+      {/* Date Picker */}
+      <FormDatePicker label="select minimum production date" name="date" />
+
+      {/* In stock */}
+      <FormCheckbox
+        label="Only products in stock"
+        name="stock"
+        defaultValue="false"
+      />
+
+      {/* BUTTONS */}
+
+      <button
+        type="submit"
+        className="btn bg-blue-600 hover:bg-blue-500 text-white btn-sm"
+      >
+        search
+      </button>
+      <Link to="/shop?page=1" className="btn btn-primary btn-sm">
+        reset
+      </Link>
+    </Form>
+  );
+};
+
+export default Filters;
